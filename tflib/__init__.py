933c440b782d2d4dbe0c3b32db57e6d8dd5d18eb
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-02-24 12:30:58 +0530

    Bracket Missing Error
    Again another bracket missing error

--- tflib/__init__.py
+++ tflib/__init__.py
@@ -99,16 +99,16 @@ def delete_param_aliases():
 #     )
 
 def print_model_settings(locals_):
-    print "Uppercase local vars:"
+    print ("Uppercase local vars:")
     all_vars = [(k,v) for (k,v) in locals_.items() if (k.isupper() and k!='T' and k!='SETTINGS' and k!='ALL_SETTINGS')]
     all_vars = sorted(all_vars, key=lambda x: x[0])
     for var_name, var_value in all_vars:
-        print "\t{}: {}".format(var_name, var_value)
+        print ("\t{}: {}".format(var_name, var_value))
 
 
 def print_model_settings_dict(settings):
-    print "Settings dict:"
+    print ("Settings dict:")
     all_vars = [(k,v) for (k,v) in settings.items()]
     all_vars = sorted(all_vars, key=lambda x: x[0])
     for var_name, var_value in all_vars:
-        print "\t{}: {}".format(var_name, var_value)
+        print ("\t{}: {}".format(var_name, var_value))
